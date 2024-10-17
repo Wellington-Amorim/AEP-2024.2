1b483cc6c5fb8c755b579657bd53f209b2bb2361
Wellington Amorim <amorimwellington788@gmail.com>
2024-10-17 19:13:00 -0300

    Update AEP.cpp

--- C/C++/AEP/AEP.cpp
+++ C/C++/AEP/AEP.cpp
@@ -1,3 +1,5 @@
+/*SISTEMA DE SUGURANÇA
+O objetivo é criar um sistema de criptografia para uma empresa que sofre com problemas de segurança.*/ 
 //Bora codar, Ryanzinho!!!!
 #include <stdio.h>
 #include <string.h>
