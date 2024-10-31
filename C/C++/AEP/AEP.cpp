b2049b73aa4edab31ddfac4fe840df817ff6fc71
Wellington Amorim <amorimwellington788@gmail.com>
2024-10-31 15:42:57 -0300

    Update AEP.cpp
    Remoção do text_info;
    Mudança no código.

--- C/C++/AEP/AEP.cpp
+++ C/C++/AEP/AEP.cpp
@@ -10,115 +10,98 @@ O objetivo é criar um sistema de criptografia para uma empresa que sofre com pr
 // Authors: Ryan R. Silva, Wellington Amorim
 
 void menu();
-void textbackground(int newcolor);
-void textcolor(int newcolor);
-// Cabeçalho da gotoxy()
 void gotoxy(int x, int y);
-
-// É necessário pra registrar o contexto da janela atual
-text_info vActual = {0, 0, 79, 24, WHITE, WHITE, C80, 25, 80, 1, 1};
+void textcolor(int color);
+void textbackground(int color);
 
 int main() {
-	setlocale(LC_ALL,"Portuguese");
-	int i;
-	
-	for(i = 5;i<40;i++) {
-		gotoxy(i,3);
-		printf("+");
-	}
-	for(i = 4;i<16;i++) {
-		gotoxy(5,i);
-		printf("|");
-	}
-return 0;
-}
+    setlocale(LC_ALL, "Portuguese");
+    int i;
 
-// ========================================== Área de Funções ================================================
-// Função trazida do <conio.c> (precisa do <windows.h> 
-void gotoxy(int x, int y) 									//GOTOXY
-{ 
-   COORD c;
-
-   if(x < 1 || x > vActual.screenwidth || 
-      y < 1 || y > vActual.screenheight) return;
-   vActual.curx = x;
-   vActual.cury = y;
-   c.X = vActual.winleft + x - 1;
-   c.Y = vActual.wintop + y - 1;
-   SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
-}
-// -----------------------------------------------------------------------------------------------------------
-/* Menu: desenha a borda e realca a opcao selecionada, chamando o procedimento desejado
-  ao apertar enter - Uma variavel muda de valor conforme se aperta as setas, mudando o realce */
-void menu() {											//MENU
-	int op = 1, ch, i;
-	
-	// Desenha o menu
-	for(i = 5;i<40;i++) {
-		gotoxy(i,3);
+    for(i = 5;i<40;i++) {
+		gotoxy(i,2);
+		printf("=");
+		gotoxy(i,16);
 		printf("=");
 	}
-	for(i = 4;i<25;i++) {
+	for(i = 3;i<16;i++) {
 		gotoxy(5,i);
-		printf("||");
+		printf("#");
+		gotoxy(39,i);
+		printf("#");
 	}
 	
-	// Loop para ler as setinhas
-	do {
-		ch = getch();
-		if(ch == 0 || ch == 224){
-			if(getch() == 80) {
-				if(op < 4) {
-					op++;	
-				}
-				else {
-					op = 1;
-				}
-			}
-			else if(getch() == 72) {
-				if(op > 1) {
-					op--;
-				}
-				else {
-					op = 4;
-				}
-			}
+	gotoxy(8,4);
+	printf("> Selecione uma opção:");
+	
+	gotoxy(8,6);
+	printf("[ Cadastrar Usuário ]");
+	gotoxy(8,8);
+	printf("[  Alterar Usuário  ]");
+	gotoxy(8,10);
+	printf("[  Excluir Usuário  ]");
+	gotoxy(8,12);
+	printf("[  Listar Usuários  ]");
+
+    printf("\n\n\n\n\n");
+
+    return 0;
+}
+// ========================================== Área de Funções ================================================
+// Funções adicionais como menu, etc., podem ser colocadas aqui
+//------------------------------------------------------------------------------------------------------------
+void menu() {
+    int op = 1, ch, i;
+
+    // Desenha o menu
+    for (i = 5; i < 40; i++) {
+        gotoxy(i, 3);
+        printf("=");
+    }
+    for (i = 4; i < 25; i++) {
+        gotoxy(5, i);
+        printf("||");
+    }
+
+    // Loop para ler as setinhas
+    do {
+        ch = getch();
+        if (ch == 0 || ch == 224) {
+            int arrow = getch();
+            if (arrow == 80) { // seta para baixo
+                if (op < 4) {
+                    op++;
+                } else {
+                    op = 1;
+                }
+            } else if (arrow == 72) { // seta para cima
+                if (op > 1) {
+                    op--;
+                } else {
+                    op = 4;
+                }
+            }
         }
-		// Realce do texto de acordo com o valor de 'op'	
-		switch(op) {
-			case 1:
-			break;
-			
-			case 2:
-			break;
-			
-			case 3:
-			break;
-			
-			case 4:
-			break;
-		}				
-	} while(ch != 27);
+    } while (ch != 27);  // 27 é o código ASCII para a tecla ESC
 }
-// -----------------------------------------------------------------------------------------------------------
-// Função trazida do <conio.c> (precisa do <windows.h>
-void textbackground(int newcolor) 							//TEXTBACKGROUND
-{
-   CONSOLE_SCREEN_BUFFER_INFO csbi;
-
-   GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
-   SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 
-      (csbi.wAttributes & 0x0f) | (newcolor << 4));
-   vActual.attribute = (csbi.wAttributes & 0x0f) | (newcolor << 4);
+//------------------------------------------------------------------------------------------------------------
+// Função para definir a posição do cursor no console
+void gotoxy(int x, int y) {
+    COORD c;
+    c.X = x - 1;  // Subtrair 1 para se alinhar ao zero-based index do Windows
+    c.Y = y - 1;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
 }
-// -----------------------------------------------------------------------------------------------------------
-// Função trazida do <conio.c> (precisa do <windows.h>
-void textcolor(int newcolor) 								//TEXTCOLOR
-{
-   CONSOLE_SCREEN_BUFFER_INFO csbi;
-
-   GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
-   SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 
-      (csbi.wAttributes & 0xf0) | newcolor);
-   vActual.attribute = (csbi.wAttributes & 0xf0) | newcolor;
+//------------------------------------------------------------------------------------------------------------
+// Função para definir a cor do texto
+void textcolor(int color) {
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+}
+//------------------------------------------------------------------------------------------------------------
+// Função para definir a cor de fundo do texto
+void textbackground(int color) {
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),
+                            (csbi.wAttributes & 0x0F) | (color << 4));
 }
